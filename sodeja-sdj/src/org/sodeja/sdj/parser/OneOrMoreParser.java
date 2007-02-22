ce99c59f324130a2aa495297260cb3de310fa658
sungon <sungon@af40afb7-032a-0410-b6c1-d98ccd4889b2>
2007-02-22 00:37:50 +0000

    new parsers

---
+++ sodeja-sdj/src/org/sodeja/sdj/parser/OneOrMoreParser.java
@@ -0,0 +1,34 @@
+package org.sodeja.sdj.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.sodeja.functional.Pair;
+
+public class OneOrMoreParser<Tok, Res> extends AbstractParser<Tok, List<Res>> {
+
+	private final Parser<Tok, Res> internal;
+
+	public OneOrMoreParser(final Parser<Tok, Res> internal) {
+		this.internal = internal;
+	}
+	
+	@Override
+	protected List<Pair<List<Res>, List<Tok>>> executeDelegate(List<Tok> tokens) {
+		List<Tok> tempTokens = tokens;
+		List<Res> tempResult = new ArrayList<Res>();
+		for(List<Pair<Res, List<Tok>>> internalResult = internal.execute(tempTokens); 
+				! internalResult.isEmpty(); 
+				internalResult = internal.execute(tempTokens)) {
+			
+			tempResult.add(internalResult.get(0).first);
+			tempTokens = internalResult.get(0).second;
+		}
+		
+		if(tempResult.size() == 0) {
+			return EMPTY;
+		}
+		
+		return create(tempResult, tempTokens);
+	}
+}
